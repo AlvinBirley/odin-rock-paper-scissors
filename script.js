14d2925545746231ed6a9a0b87eefa778085501c
AlvinBirley <alvinbirley@gmail.com>
2024-09-26 16:17:39 -0500

    Create once efficient user input function to replace previous three

--- script.js
+++ script.js
@@ -5,28 +5,12 @@ let userScore = 0
 //Declare computer score variable
 let compScore = 0 
 
+//Selecting all 3 buttons
+const allBtns = document.querySelectorAll('.my-button');
+
 
 //Computer choice array
 let rpsArray = ["rock", "paper", "scissors"];
-
-//Selecting the rock button
-const rockBtn = document.getElementById("rock");
-//Selecting inner HTML of rock button 
-const rockBtnTxt = rockBtn.innerHTML;
-//console.log(rockBtnTxt)
-
-//Selecting the paper button
-const paperBtn = document.getElementById("paper");
-//Selecting inner HTML of paper button 
-const paperBtnTxt = paperBtn.innerHTML;
-//console.log(paperBtnTxt)
-
-//Selecting the scissors button
-const scissorsBtn = document.getElementById("scissors");
-//Selecting inner HTML of scissors button 
-const scissorsBtnTxt = scissorsBtn.innerHTML;
-//console.log(scissorsBtnTxt)
-
 
 
 //Create function that randomizes and returns computer choice
@@ -36,12 +20,10 @@ function getComputerChoice () {
 console.log(getComputerChoice())
 
 //Create function that takes the user choice
-
-function getUserChoice(choice){
-console.log(choice)
-}
-
-function getHumanChoice (){
-}
-
+allBtns.forEach(allBtns => {
+allBtns.addEventListener("click", function getUserChoice(){
+    const getValue = this.innerHTML
+    console.log(getValue)
+});
+});
 
