a782f244b5d6d103c962ad255b0af8392d0ba001
AlvinBirley <alvinbirley@gmail.com>
2024-09-30 16:32:23 -0500

    Create function that returns user choice and computer choice

--- script.js
+++ script.js
@@ -1,29 +1,46 @@
-
+//Select div with the id of "score"
+const scoreDiv = document.getElementById("score")
 //Declare user score variable 
 let userScore = 0 
 
 //Declare computer score variable
 let compScore = 0 
 
-//Selecting all 3 buttons
-const allBtns = document.querySelectorAll('.my-button');
-
+//User choice stand-in for now
+let word = "user choice"
 
 //Computer choice array
 let rpsArray = ["rock", "paper", "scissors"];
 
-
-//Create function that randomizes and returns computer choice
+//Function to get computer choice
 function getComputerChoice () {
     return rpsArray[Math.floor(Math.random()* rpsArray.length)]
 }
-console.log(getComputerChoice())
 
-//Create function that takes the user choice
-allBtns.forEach(allBtns => {
-allBtns.addEventListener("click", function getUserChoice(){
-    const getValue = this.innerHTML
-    console.log(getValue)
-});
-});
+//Declare variable to store computer choice function
 
+let lastUserChoice = ""
+
+
+function playRound(humanChoice, computerChoice){
+    console.log(humanChoice)
+    console.log(computerChoice)
+    }
+    
+
+    function getButtonValue(event) {
+        return event.target.value; 
+    }
+    
+
+    function handleButtonClick(event) {
+        const buttonValue = getButtonValue(event); 
+       const lastUserChoice = buttonValue
+       let robotChoice = getComputerChoice();
+       playRound(lastUserChoice, robotChoice)
+    }
+    
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach(button => {
+        button.addEventListener('click', handleButtonClick);
+    });
